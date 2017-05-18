3491e24307b1852dd4e9a38e70d9f061d316e4b1
mrbeean_HP <MrSitiZ@yandex.ru>
2017-05-18 12:54:25 +0300

    add user validations & connect with subscription

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,25 @@ class User < ActiveRecord::Base
   # У юреза должно быть имя не длиннее 35 букв
   validates :name, presence: true, length: {maximum: 35}
 
+  validates :email, presence: true, length: {maximum: 255}
+  validates :email, uniqueness: true
+  validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+
   # При создании нового юзера (create), перед валидацией объекта выполнить
   # метод set_name
   before_validation :set_name, on: :create
+
+  after_commit :link_subscriptions, on: :create
 
   private
 
   # Задаем юзеру случайное имя, если оно пустое
   def set_name
     self.name = "Товарисч №#{rand(777)}" if self.name.blank?
+  end
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email)
+      .update_all(user_id: self.id)
   end
 end
